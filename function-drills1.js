7a4f3e5054a4ca1e8459d4affce0326c6b55580f
Catrina Fuentes <cat.fuentes93@gmail.com>
2019-12-02 13:29:46 -0800

    Added try/catch blocks and if/else statement to function getYearOfBirth

--- function-drills1.js
+++ function-drills1.js
@@ -1,10 +1,21 @@
 function getYearOfBirth (age) {
-    return yearOfBirth;
+    if (age < 0) {
+        throw new Error ("Age can not be negative");
+    }
+    else {
+        return yearOfBirth;
+    }
 }
 
 function createGreeting (name, age) {
     const yearOfBirth = 2019 - age;
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearofBirth}.`;
+}
+
+try {
+    const greeting1 = createGreeting(Joe, -50); 
+} catch(e) {
+    console.log("Age can not be negative");
 }
 
 const greeting1 = createGreeting();
