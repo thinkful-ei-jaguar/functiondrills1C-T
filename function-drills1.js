32f12fa5c6f2f4231d0fa2b20e5a98472b90d28b
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-02 16:19:07 -0500

    created getYearOfBirth function

--- function-drills1.js
+++ function-drills1.js
@@ -1,3 +1,7 @@
+function getYearOfBirth (age) {
+    return yearOfBirth;
+}
+
 function createGreeting (name, age) {
     const yearOfBirth = 2019 - age;
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearofBirth}.`;
