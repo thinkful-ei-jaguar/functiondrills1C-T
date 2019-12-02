32abf9d2aba5e3acb38c1c2491718cddcacdffc1
Catrina Fuentes <cat.fuentes93@gmail.com>
2019-12-02 14:01:22 -0800

    Compared solution, made notes for reference on syntax errors.

--- function-drills1.js
+++ function-drills1.js
@@ -1,15 +1,17 @@
 function getYearOfBirth (age) {
     if (age < 0) {
         throw new Error ("Age can not be negative");
-    }
+    } // this throw statement should be in createGreeting //
     else {
         return yearOfBirth;
-    }
+    } //should return the calculation of year - age parameter//
 }
 
 function createGreeting (name, age) {
     const yearOfBirth = 2019 - age;
-    if (name == 'undefined' && age == 'undefined') {
+    if (name == 'undefined' && age == 'undefined')
+    // name and age used || statement, undefined doesn't have quotes around it. //  
+    {
         throw new Error ("Arguments not valid");
     }
     else if (age !== NaN && name != string) {
@@ -18,12 +20,14 @@ function createGreeting (name, age) {
     else {
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearofBirth}.`;
     }
-}
+} // make const variable first 1 function to use it in this function, then return template literal//
 
 try {
     const greeting1 = createGreeting(Joe, -50); 
+    //add console.log of const variable//
 } catch(e) {
     console.log("Age can not be negative");
+    //e.message//
 }
 
 const greeting1 = createGreeting();
